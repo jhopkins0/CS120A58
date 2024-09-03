ba252ecc0fd6d37e95e93718edc3ccf8888f27e9
jhopkins0 <156948029+jhopkins0@users.noreply.github.com>
2024-09-03 11:54:32 -0700

    iostream not working

--- main_write.cpp
+++ main_write.cpp
@@ -4,7 +4,23 @@ using namespace std;
 
 int main()
 {
-   /***************************************************
-    * Code your program here
-    ************************************************** */
+  int N;
+   string sname;
+   int score1, score2;
+   ifstream ifs;
+
+   ifs.open("students.txt");
+   if (!ofs){
+    cerr << "File Open Error\n";
+    exit(0);
+   }
+   cout << "Enter the number of students: "
+   cin >> N;
+   ofs << N << endl;
+   for(int i = 0; i < N; i++){
+    cout << "Enter the student name: ";
+    cin >> sname;
+    cout << "Enter the student's scores: ";
+    cin >> score1 >> score2;
+    ofs << sname << "\t" << score1 << "\t" << score2;
 }
